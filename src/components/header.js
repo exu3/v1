bd8becc1ab0e2e33ec20cfd29f274e45f4ceaf8d
eilla1 <exu6056@gmail.com>
2021-02-12 14:57:10 -0800

    :sparkles: create header

---
+++ src/components/header.js
@@ -0,0 +1,28 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import headerStyles from './header.module.scss'
+
+const Header = () => {
+    return (
+        <header className={headerStyles.header}>
+            <h1>
+                <Link className={headerStyles.title} to="/">
+                    Ella
+                </Link>
+            </h1>
+            <nav>
+                <ul className={headerStyles.navList}>
+                    <li>
+                        <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/projects">Projects</Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
