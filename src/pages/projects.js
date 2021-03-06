5d43d168ed09f15a147b2bcda0498401c967628e
eilla1 <exu6056@gmail.com>
2021-03-05 21:43:22 -0800

    :sparkles: create project cards

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,14 +1,24 @@
 import React from "react"
 import Layout from '../components/layout'
 import Head from '../components/head'
-import { Text } from '@chakra-ui/react'
+import ProjectCard from '../components/projectcard'
+import { Text, Badge } from '@chakra-ui/react'
 
 const ProjectsPage = () => {
     return (
         <Layout>
             <Head />
-            <Text fontSize="4xl" fontWeight="extrabold">Projects</Text>
-            <Text>currently a wip... if you're curious, you can reach me via email exu6056 at g mail dot com or alternatively, I am also on the Hack Club Slack as <a href="https://slack.hackclub.com">Ella</a>.</Text>
+            <Text fontSize="4xl" fontWeight="extrabold">Projects</Text> <br />
+            <ProjectCard 
+                image="https://cloud-f2pye8tve-hack-club-bot.vercel.app/0image.png" 
+                alt="dog" 
+                title="Title Test" 
+                date="February 2021" 
+                blurb="This is a test test test is an English-language pangram—a
+                sentence that contains all of the letters of the English alphabet. Owing to
+                its existence, Chakra was created."
+                link="#"
+                />
         </Layout>
     )
 }
