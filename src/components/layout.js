b6c59faa50454ff407208a6e005543d5b1240127
eilla1 <exu6056@gmail.com>
2021-03-05 21:42:59 -0800

    :fire: change font

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,19 +1,22 @@
 import React from 'react'
 import Header from './header'
 import Footer from './footer'
+import { ChakraProvider } from "@chakra-ui/react"
 
 import '../styles/index.scss'
 import layoutStyles from './layout.module.scss'
 
 const Layout = (props) => {
     return (
-        <div className={layoutStyles.container}>
-            <div className={layoutStyles.content}>
-                <Header />
-                {props.children} {/* without props, it renders an empty div */}
+        <ChakraProvider>
+            <div className={layoutStyles.container}>
+                <div className={layoutStyles.content}>
+                    <Header />
+                    {props.children} {/* without props, it renders an empty div */}
+                </div>
+                <Footer />
             </div>
-            <Footer />
-        </div>
+        </ChakraProvider>
     )
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,16 +2,27 @@ import React from 'react';
 import { Link } from 'gatsby';
 import Layout from '../components/layout'
 import Head from '../components/head'
-import { Text } from '@chakra-ui/react'
+import { Text, Box, Flex, Image } from '@chakra-ui/react'
 
 const IndexPage = () => {
   return (
     <Layout>
       <Head />
-      <Text fontSize="4xl" fontWeight="extrabold">Hey, I'm Ella.</Text>
-      <Text>Nice to e-meet you! I'm a high school junior from California, and I'm learning how to code. I love making cool things on the web! In my very limited free time, you can find me hanging out with my chickens, spending time outdoors, or staring out the window.</Text>
-      <Text>Check out what I'm up to <Link to="/projects">here.</Link></Text>
-    </Layout>
+      <Text fontSize="4xl" fontWeight="extrabold">Hey, I'm Ella.</Text><br />
+        <Flex>
+          <Box>
+            <Text>
+              Nice to e-meet you. I'm a high school junior from the Bay Area. At (Zoom) school, I hangout in several clubs including engineering club, Interact, NHS, among others. Outside of school, I love working on side projects and participating in hackathons. Although my experience primarily lies in front-end web development, I am also interested in hardware, UI/UX design, and data science. As of February 2021, I have participated in almost a dozen hackathons and won an award at three of them.
+            </Text><br />
+            <Text>
+              When I'm not coding or doing homework, I like to hike in the woods behind my neighborhood and spend time in nature. I also enjoy growing plants and hanging out with my chickens.
+            </Text>
+          </Box>
+          <Box>
+            <Image src="https://cloud-1dbtgci6g-hack-club-bot.vercel.app/0wheeeeeeeeeeeeee.png" alt="Person looking at phone" />
+          </Box>
+        </Flex>
+      </Layout>
   )
 }
 
