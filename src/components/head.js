f73355642ce4e4e0ce08f96388a92c81ca522bb1
eilla1 <exu6056@gmail.com>
2021-03-08 22:38:35 -0800

    :mag: Add site metadata

--- src/components/head.js
+++ src/components/head.js
@@ -3,7 +3,19 @@ import { Helmet } from 'react-helmet'
 
 const Head = () => {
     return (
-        <Helmet title="Ella" />
+        <Helmet>
+            <title>Ella | Portfolio</title>
+            <meta name="description" content="Hey, I'm Ella, a high school student from California and an aspiring software developer." />
+
+            {/* OpenGraph tags */}
+            <meta property="og:url" content="https://ela.now.sh" />
+            <meta property="og:title" content="Ella | Portfolio" />
+            <meta property="og:description" content="Hey, I'm Ella, a high school student from California and an aspiring software developer." />
+
+            {/* Twitter Card tags */}
+            <meta name="twitter:title" content="Ella | Portfolio" />
+            <meta name="twitter:description" content="Hey, I'm Ella, a high school student from California and an aspiring software developer." />
+        </Helmet>
     )
 }
 
